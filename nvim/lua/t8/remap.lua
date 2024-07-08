a1b11b0ec836a193055e87bafcab7c81455d5286
Taiterbase <taitenazifi@gmail.com>
2024-07-08 10:29:46 -0700

    refactor(nvim): Update nvim-treesitter commit and keybindings

--- nvim/lua/t8/remap.lua
+++ nvim/lua/t8/remap.lua
@@ -1,8 +1,14 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<M-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<M-k>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<M-j>", ":m .+1<CR>==")
+vim.keymap.set("n", "<M-k>", ":m .-2<CR>==")
+vim.keymap.set("v", "<M-S-j>", ":'<,'>t'<-1<CR>gv=gv<Esc>")
+vim.keymap.set("v", "<M-S-k>", ":'<,'>t'<+1<CR>gv=gv<Esc>")
+vim.keymap.set("n", "<M-S-j>", ":t.<CR>==")
+vim.keymap.set("n", "<M-S-k>", ":t-1.<CR>==")
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -30,10 +36,10 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/t8/packer.lua<CR>")
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<C-t>", "tabn", { silent = true })
 vim.keymap.set("n", "<C-St>", ":tabp")
 vim.keymap.set("n", "<ESC>", "<cmd>nohlsearch<CR>")
 
-
+vim.keymap.set("i", "<D-BS>", "<Esc>d0i")
