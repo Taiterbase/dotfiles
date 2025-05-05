7b38768f651612590bdbdf06ceb3955e018c6390
Taite Nazifi <taitenazifi@gmail.com>
2025-05-04 21:25:46 -0700

    fix(nvim): gitlinker works now

--- nvim/lua/t8/lazy/gitlinker.lua
+++ nvim/lua/t8/lazy/gitlinker.lua
@@ -1,4 +1,18 @@
 return {
     "ruifm/gitlinker.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        require("gitlinker").setup({
+            mappings = "gy",
+            callbacks = {
+                [".*"] = function(url_data)
+                    url_data.host = "github.com"
+                    return require("gitlinker.hosts").get_github_type_url(url_data)
+                end,
+                ["github.com"] = require("gitlinker.hosts").get_github_type_url,
+                ["gitlab.com"] = require("gitlinker.hosts").get_gitlab_type_url,
+                ["bitbucket.org"] = require("gitlinker.hosts").get_bitbucket_type_url,
+            },
+        })
+    end,
 }
